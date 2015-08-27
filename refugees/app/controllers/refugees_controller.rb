a35582078c9a979e697028c035134b4854f9d519
Sonju Walker <sonjuwalker@gmail.com>
2015-08-27 15:39:50 -0500

    routers created

--- refugees/app/controllers/refugees_controller.rb
+++ refugees/app/controllers/refugees_controller.rb
@@ -1,13 +1,36 @@
-class RefugeesController < ApplicationController
-  def index
+class Api::RefugeesController < ApplicationController
+
+  def list
+    # return all refugees in an []
+    @refugees = Refugee.all
   end
 
-  def show
+  def create
+  # create refugees
+  puts '-----START PARAMS-----'
+  puts params
+  puts '-----END PARAMS-----'
+  @refugee = Refugee.create(
+    :country_of_origin => params[:country_of_origin],
+    :territory_country_of_asylum  => params[:territory_country_of_asylum],
+    :refugees  => params[:refugees],
+    :year  => params[:year]
+  )
+    render json: @refugee
   end
 
-  def edit
+  def read
+    # return an individual refugee
+    @refugee = Refugee.find(params[:id]).read
   end
 
-  def new
+  def update
+    # update refugee by id
+    @refugee = Refugee.find(params[:id]).update
+  end
+
+  def destroy
+    # delete refugee by id
+    @refugee = Refugee.find(params[:id]).destory
   end
 end
